fa95e204df14d654dab121198459b08668ac46ae
Pasha <93393767+VladanPasagic@users.noreply.github.com>
2024-04-04 23:17:17 +0200

    Finished logic for custom file input component

--- src/app/Components/file-input/file-input.component.ts
+++ src/app/Components/file-input/file-input.component.ts
@@ -1,17 +1,21 @@
 import { Component, Input } from '@angular/core';
+import { FormGroup, ReactiveFormsModule } from '@angular/forms';
 import { MatCardModule } from '@angular/material/card';
 
 @Component({
   selector: 'app-file-input',
   standalone: true,
-  imports: [MatCardModule],
+  imports: [MatCardModule, ReactiveFormsModule],
   templateUrl: './file-input.component.html',
   styleUrl: './file-input.component.css',
 })
 export class FileInputComponent {
   @Input() text!: string;
-  @Input() value!: File;
+  @Input() formControlNameString!: string;
+  @Input() form!: FormGroup;
+
   public triggerFileInput() {
     document.getElementById('fileinput')?.click();
+    console.log(this.form.value.avatar);
   }
 }
